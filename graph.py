06c2d56f5d6f6efae82f1e51c8aa866f3f791846
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-06 10:48:24 -0700

    finished adjacency matrix graph class

--- graph.py
+++ graph.py
@@ -72,4 +72,36 @@ class AdjacencyMatrixGraph(Graph):
 
         return adjacent_vertices
 
-        
+    def get_indegree(self, v):
+        if v < 0 or v >= self.numVertices:
+            raise ValueError("Cannot access vertex %d" % v) 
+            
+        indegree = 0
+        for i in range(self.numVertices):
+            if self.matrix[i][v] > 0:
+                indegree = indegree + 1
+        return indegree
+
+    def get_edge_weight(self, v1, v2):
+        return self.matrix[v1][v2]
+           
+    def display(self):
+        for i in range(self.numVertices):
+            for v in self.get_adjacent_vertices(i):
+                print(i, "-->", v)
+
+g = AdjacencyMatrixGraph(4)
+
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(2, 3)
+
+for i in range(4):
+    print("Adjacent to: ", i, g.get_adjacent_vertices(i))
+for i in range(4):
+    print("Indegree: ", i, g.get_indegree(i))
+for i in range(4):
+    for j in g.get_adjacent_vertices(i):
+        print("Edge weight: ", i, " ", j, " weight: ", g.get_edge_weight(i, j))
+g.display()
+
